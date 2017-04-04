ad0a319ae479439cf1659019cdb13c4d50877a5f
matsduf <mats.dufberg@iis.se>
2017-04-04 12:32:48 +0200

    Update CLI.pm
    Updated version of CLI.pm for upcoming release.

--- lib/Zonemaster/CLI.pm
+++ lib/Zonemaster/CLI.pm
@@ -6,7 +6,7 @@ extends 'Zonemaster::Exception';
 # The actual interesting module.
 package Zonemaster::CLI;
 
-use version; our $VERSION = version->declare("v1.0.8");
+use version; our $VERSION = version->declare("v1.1.0");
 
 use 5.014002;
 use warnings;
