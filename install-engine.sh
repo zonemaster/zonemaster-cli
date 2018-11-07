6cd28828ca5742a28eb10a5a2e097de929c9e986
Mattias Päivärinta <mattias.paivarinta@iis.se>
2018-11-07 15:38:25 +0100

    Take Zonemaster deps from develop branch

---
+++ install-engine.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -ex
+
+cd /tmp
+wget -O zonemaster-ldns.zip https://github.com/zonemaster/zonemaster-ldns/archive/develop.zip
+unzip zonemaster-ldns.zip
+cd zonemaster-ldns-develop
+perl Makefile.PL
+make
+sudo make install
+perl -MZonemaster::Engine -E 'say $Zonemaster::LDNS::VERSION'
+
+cd /tmp
+wget -O zonemaster-engine https://github.com/zonemaster/zonemaster-engine/archive/develop.zip
+unzip zonemaster-engine.zip
+cd zonemaster-engine-develop
+perl Makefile.PL
+make
+sudo make install
+perl -MZonemaster::Engine -E 'say $Zonemaster::Engine::VERSION'
