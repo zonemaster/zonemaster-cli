b6a9f5e5cc7ff643b9549b7f5747e57772ac8c29
Mats Dufberg <mats.dufberg@iis.se>
2020-05-18 16:37:40 +0200

    Preparation for release v2019.2.2 (Zonemaster-CLI version v2.0.4).

--- lib/Zonemaster/CLI.pm
+++ lib/Zonemaster/CLI.pm
@@ -6,7 +6,7 @@ extends 'Zonemaster::Engine::Exception';
 # The actual interesting module.
 package Zonemaster::CLI;
 
-use version; our $VERSION = version->declare("v2.0.3");
+use version; our $VERSION = version->declare("v2.0.4");
 
 use 5.014002;
 use warnings;
